d8968f18a6fad7711fc8bb089a405b79103d0676
ConAntares <LukeNiu@outlook.com>
2019-03-01 03:17:20 +0800

    change

---
+++ Practice/pi.jl
@@ -0,0 +1,15 @@
+# Calculate π using Taylor Series
+
+o = time()
+s = 0.0
+
+for n in 0:10000000
+#    println(((-1)^n//(2n+1)))
+    global s = s + (-1)^n/(2n+1)
+end
+
+pi = 4s
+t = time()
+
+println(pi)
+println("Time Interval: ",t-o," second")
---
+++ Practice/pi.py
@@ -0,0 +1,16 @@
+# Calculate Ï€ using Taylor Series
+
+import time
+
+o = time.time()
+s = 0.0
+n = 0
+
+for n in range(10000000):
+    s = s + (-1)**n/(2*n+1)
+
+pi = 4*s
+t = time.time()
+
+print(pi)
+print("Time Interval:",t-o,"second")
