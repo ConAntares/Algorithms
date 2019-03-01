979085aedfd75e7811e3c68f1971ea38ea98732f
ConAntares <LukeNiu@outlook.com>
2019-03-01 19:09:03 +0800

    change

--- Practice/e.jl
+++ Practice/e.jl
@@ -1,13 +1,16 @@
 # Calculate e using Taylor Series
 
+o = time()
 s = 1.0
 f = 1.0
 
-for n in 1:100
+for n in 1:1000
     global f = n * f
     global s = s + 1/f
 end
 
 e = s
+t = time()
 
 println(e)
+println("Time Interval: ",t-o," second")
