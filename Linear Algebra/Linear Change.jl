0cbbe9faa061920035de610d9f7afbc4993f8f33
LukeNiu@outlook.com <LukeNiu@outlook.com>
2019-03-29 03:05:53 +0800

    change

--- Linear Algebra/Linear Change.jl
+++ Linear Algebra/Linear Change.jl
@@ -4,12 +4,13 @@
 # y=y_min+(x-x_min)(y_max-y_min)/(x_max-x_min)
 
 X = range(0,stop=10,step=1)
+println("X:")
 for n in 1:11
     print(X[n],"\t")
 end
 println()
 
-println("Y[n]=4*X[n]+20")
+println("Y[n]=4*X[n]+20:")
 Y = [4*X[n]+10 for n in 1:11]
 for n in 1:11
     print(Y[n],"\t")
---
+++ Linear Algebra/N-ary Arrangement.jl
@@ -0,0 +1,19 @@
+#### N-ary Arrangement
+
+# Input a Vector as Sequence:
+A = [1,2,3,4,5,4,3,2,1]
+println("The sequence is ",A)
+
+# Print the elements number of the Vector:
+println("The number of elements is ",length(A))
+
+# initialize the Order Number:
+o = Int64(0)
+
+# initialize the Reverse Order Number:
+t = Int64(0)
+
+# Calculus the Order Number:
+for i in 1:length(A)
+    for j in 1:
+
