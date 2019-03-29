aac7279f81d9ddd13954c3905a9bc37bc17306ae
LukeNiu@outlook.com <LukeNiu@outlook.com>
2019-03-30 01:09:13 +0800

    change

--- Linear Algebra/Expand Determinant by Row.jl
+++ Linear Algebra/Expand Determinant by Row.jl
@@ -1,26 +1,33 @@
 #### Expand Determinant by Row
 
-using LinearAlgebra
-
 # Input a n*n array as the determinant you want to analysis.
-A = 
+D = 
 [
-    4 2 3 5; 
-    3 4 6 3;
-    2 7 5 4;
-    5 3 4 3
+    4 2 3;
+    3 4 6;
+    2 7 5;
 ]
-# The number of column
-nCol = size(A,2)
 # The number of row
-nRow = size(A,1)
+nRow = size(D,1)
+# The number of column
+nCol = size(D,2)
 
-M = 5
-# Expand the determinant by the first row
-for c = 1:nCol
-    for i = 1:nRow
-        for j = 1:nCol
-            if n<
-                global M[i,j]=A[i-1,]
+# Initialize the remainder
+M = zeros(nRow-1,nCol-1)
+# Initialize algebraic remainder
+A = zeros(nRow-1,nCol-1)
+
+for n = 1:nCol              # D[1,1], D[1,2], ... , D[1,n]
+    for i = 1:nRow-1
+        for j = 1:nCol-1
+            if n <= j
+                global M[i,j] = D[i+1,j+1]
+            elseif n > j
+                global M[i,j] = D[i+1,j]
+            end
+        end
+    end
+    A = (-1)^(1+n)*M
+    println("The of remainder D[$n] is $M")
+    println("The of algebraic remainder D[$n] is $A")
 end
-
