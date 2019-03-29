8c2dfa06450527ab4b291245e47107c5d34f8681
LukeNiu@outlook.com <LukeNiu@outlook.com>
2019-03-30 01:14:31 +0800

    c

--- Linear Algebra/Expand Determinant by Row.jl
+++ Linear Algebra/Expand Determinant by Row.jl
@@ -14,9 +14,10 @@ nCol = size(D,2)
 
 # Initialize the remainder
 M = zeros(nRow-1,nCol-1)
-# Initialize algebraic remainder
+# Initialize the algebraic remainder
 A = zeros(nRow-1,nCol-1)
 
+# Assign values to a the remainder and algebraic remainder
 for n = 1:nCol              # D[1,1], D[1,2], ... , D[1,n]
     for i = 1:nRow-1
         for j = 1:nCol-1
