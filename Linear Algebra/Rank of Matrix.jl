a0c41c017b3f3490b4d60d601b0c4ac040c37ec6
LukeNiu@outlook.com <LukeNiu@outlook.com>
2019-04-02 05:36:23 +0800

    fix

--- Linear Algebra/Rank of Matrix.jl
+++ Linear Algebra/Rank of Matrix.jl
@@ -7,26 +7,26 @@ D =
 [
     4 2 3;
     3 4 6;
-    2 7 5;
+    3 4 6;
 ]
 # Then the Rd is the rank of your matrix:
 Rd = rank(D)
 println("The rank of your matrix is $Rd")
 
 # Input a 1*n array as the vector of constant term.
-C = [1,2,3]
+C = [1,2,2]
 # The augmented matrix is:
 A = [D C]
 # The rank of the augmented:
 Ra = rank(A)
 println("The rank of your augmented matrix is $Ra")
 
-if Ra == Rd
+if Rd == Ra
     if Rd == size(D,2)
         println("The equation set which determined by matrix A has a solution.")
     elseif Rd < size(D,2)
         println("The equation set which determined by matrix A has infinite solutions.")
     end
-elseif Ra < Rd
+elseif Rd < Ra
     println("The equation set which determined by matrix A has no solution.")
 end
