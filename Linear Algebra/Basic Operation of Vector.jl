a85cbe140bd5565d1a4f44ee8cb9dcbfec79096e
ConAntares <LukeNiu@outlook.com>
2019-04-05 11:49:47 +0800

    update

---
+++ Linear Algebra/Basic Operation of Vector.jl
@@ -0,0 +1,40 @@
+#### Basic Operation of Vector
+
+# Input a 1*n array as the matrix you want to analysis.
+A = [1,2,3]
+B = [4,6,8]
+
+# Vector Addition:
+Va = A+B
+    # [5, 8, 11]
+
+# Vector Subtraction:
+Vs = A-B
+    # [-3, -4, -5]
+
+# Scalar-Vector Multiplication:
+Vc = 1.2*A
+    # [1.2, 2.4, 3.6]
+
+# Scalar-Vector Division:
+Vd = A/1.2
+     # [0.833333, 1.66667, 2.5]
+
+# Scalar-Vector addition:
+Vr = A.+3.0
+    # [4.0, 5.0, 6.0]
+
+# Elements Product:
+Vp = A.*B
+    # [4, 12, 24]
+
+# Inner Product:
+Vi = A'*B
+    # 40
+
+# Cross Product:
+Vo = A*B'
+    # 3×3 Array{Int64,2}:
+    #   4   6   8
+    #   8  12  16
+    #  12  18  24
