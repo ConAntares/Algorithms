1bcc53c1444003ab952bcb1b71a155db94142bc3
LukeNiu@outlook.com <LukeNiu@outlook.com>
2019-04-10 23:05:51 +0800

    update

--- Linear Algebra/Solve Linear Equations.jl
+++ Linear Algebra/Solve Linear Equations.jl
@@ -1,1 +1,7 @@
 #### Solve Linear Equations
+
+using LinearAlgebra
+
+# Input a n*m array as the coefficient term matrix:
+
+# Input a 1*m array as the constant term matrix:
--- Linear Algebra/Solve Linear Equations.py
+++ Linear Algebra/Solve Linear Equations.py
@@ -1,1 +1,8 @@
 #### Solve Linear Equations
+
+#%%
+import numpy as np
+
+# Input a n*m array as the coefficient term matrix:
+
+# Input a 1*m array as the constant term matrix:
