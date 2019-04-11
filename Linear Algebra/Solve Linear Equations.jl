be02efa6ba0b2a139c9069b252a68a146f356d2d
LukeNiu@outlook.com <LukeNiu@outlook.com>
2019-04-11 15:02:03 +0800

    Update

--- Linear Algebra/Solve Linear Equations.jl
+++ Linear Algebra/Solve Linear Equations.jl
@@ -7,12 +7,25 @@ In general, a linear equation system can be written on the form:
 
 using LinearAlgebra
 
-# Input a m*n array as the coefficient term matrix:
+# Input a m*n array as the coefficient term matrix
 A = [1 6 4 3;
      6 2 1 9;
      5 4 6 6;
      4 5 2 5;]
-# Input a m*1 array as the constant term matrix:
+
+# Input a m*1 array as the constant term matrix
 B = [4;3;6;1]
-# The augmented matrix:
+# The augmented matrix
 J = hcat(A,B)
+# A zeros vector:
+Z = zeros(size(B,1),1)
+if iszero(B) == true
+    println("The original equation set is homogeneous.")
+elseif iszero(B) == false
+    println("The original equation set is not homogeneous.")
+end
+
+# The rank of coefficient term matrix
+Ra = rank(A)
+# Calculate the value of determinant
+Da = det(A)
--- Linear Algebra/Solve Linear Equations.py
+++ Linear Algebra/Solve Linear Equations.py
@@ -5,15 +5,31 @@ In general, a linear equation system can be written on the form:
     A(m,n)*X(n*1)=B(m*1)
 """
 
-#%%
 import numpy as np
 
-# Input a m*n array as the coefficient term matrix:
+# Input a m*n array as the coefficient term matrix
 A = np.array([[1,6,4,3],
               [6,2,1,9],
               [5,4,6,6],
               [4,5,2,5]])
-# Input a m*1 array as the constant term matrix:
+
+# Input a m*1 array as the constant term matrix
 B = np.array([[4],[3],[6],[1]])
-# The augmented matrix:
+# The augmented matrix
 J = np.hstack((A,B))
+# A zeros vector
+Z = np.zeros((B.shape[0],1))
+if B.any() == 0:
+    print("The original equation set is homogeneous.")
+elif B.any() != 0:
+    print("The original equation set is not homogeneous.")
+
+# The rank of coefficient term matrix
+Ra = np.linalg.matrix_rank(A)
+# Calculate the value of determinant
+Da = np.linalg.det(A)
+
+if Da != 0:
+    print("The original equations are homogeneous.")
+elif Da == 0:
+    if 
