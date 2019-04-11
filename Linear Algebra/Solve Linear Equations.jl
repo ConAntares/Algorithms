dee37db7c18393550438c29e33f6a4bcebf90f52
LukeNiu@outlook.com <LukeNiu@outlook.com>
2019-04-11 13:10:26 +0800

    update

--- Linear Algebra/Solve Linear Equations.jl
+++ Linear Algebra/Solve Linear Equations.jl
@@ -8,13 +8,11 @@ In general, a linear equation system can be written on the form:
 using LinearAlgebra
 
 # Input a m*n array as the coefficient term matrix:
-A =
-[
-    1 6 4 3;
-    6 2 1 9;
-    5 4 6 6;
-    4 5 2 5;
-]
-
+A = [1 6 4 3;
+     6 2 1 9;
+     5 4 6 6;
+     4 5 2 5;]
 # Input a m*1 array as the constant term matrix:
 B = [4;3;6;1]
+# The augmented matrix:
+J = hcat(A,B)
--- Linear Algebra/Solve Linear Equations.py
+++ Linear Algebra/Solve Linear Equations.py
@@ -9,5 +9,11 @@ In general, a linear equation system can be written on the form:
 import numpy as np
 
 # Input a m*n array as the coefficient term matrix:
-
+A = np.array([[1,6,4,3],
+              [6,2,1,9],
+              [5,4,6,6],
+              [4,5,2,5]])
 # Input a m*1 array as the constant term matrix:
+B = np.array([[4],[3],[6],[1]])
+# The augmented matrix:
+J = np.hstack((A,B))
