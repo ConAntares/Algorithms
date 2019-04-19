6cd61f61b9d54e50456c314395a3821999f023a5
ConAntares <LukeNiu@outlook.com>
2019-04-19 19:37:05 +0800

    update

--- Numerical Methods/Lagrange Interpolation Formula.py
+++ Numerical Methods/Lagrange Interpolation Formula.py
@@ -1,1 +1,17 @@
 #### Lagrange Interpolation Formula
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange
+
+def lagrange_interpolate(x, y, t):
+    p = lagrange(x, y)
+    u = p(t)
+
+    plt.plot(x, y, "bo")
+    plt.plot(t, u, "ro")
+    plt.show()
+
+x, y = np.loadtxt('Numerical Methods/Data/data02.dat', unpack=True)
+t = np.linspace(1, 2, 10)
+lagrange_interpolate(x, y, t)
