6e0951d14513c8465c3df6083dbeed86dc89f3c3
ConAntares <LukeNiu@outlook.com>
2019-05-10 04:06:08 +0800

    Update

--- Approach/Set in Python.py
+++ Approach/Set in Python.py
@@ -10,3 +10,21 @@ print(type(re))
     # <class 'set'>
 print(len(re))
     # 5
+re.add(6)
+print(re)
+    # {1, 2, 3, 4, 5, 6}
+re.remove(6)
+print(re)
+    # {1, 2, 3, 4, 5}
+
+A = set([1,2,3,4])
+B = set([1,3,5,7])
+re = A & B
+print(re)
+    # {1, 3}
+re = A | B
+print(re)
+    # {1, 2, 3, 4, 5, 7}
+re = A^B
+print(re)
+    # {2, 4, 5, 7}
