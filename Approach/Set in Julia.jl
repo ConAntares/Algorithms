6f2d94aae7b0c0f468520a2e2e9005b4e950d44f
ConAntares <LukeNiu@outlook.com>
2019-05-10 03:50:15 +0800

    Update

--- Approach/Set in Julia.jl
+++ Approach/Set in Julia.jl
@@ -7,3 +7,13 @@ println(re)
 re = Set((1, 2, 3, 4, 5, 2, 4))
 println(re)
     # Set([4, 2, 3, 5, 1])
+
+A = Set([1,2,3,4])
+B = Set([1,3,5,7])
+
+re = intersect(A,B)
+println(re)
+    # Set([3, 1])
+re = union(A,B)
+println(re)
+    # Set([7, 4, 2, 3, 5, 1])
