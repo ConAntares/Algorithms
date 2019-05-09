49129c7ffa830756bd651fd7a427fa11d86a71d5
ConAntares <LukeNiu@outlook.com>
2019-05-09 23:51:45 +0800

    Update

--- Approach/Vector Martix Tensor in Python.py
+++ Approach/Vector Martix Tensor in Python.py
@@ -3,3 +3,15 @@
 import numpy as np
 
 ## Vector
+print('\n Vector in NumPy:')
+υ = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(υ)
+print(υ.dtype)
+print(υ.ndim)
+print(υ.shape)
+print(υ.size)
+print(υ.nbytes)
+κ = np.arange(0.0,10,2)
+print(κ)
+η = np.linspace(0,10,6)
+print(η)
