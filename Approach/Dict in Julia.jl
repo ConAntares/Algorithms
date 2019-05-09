a0c296fb724b4b4074c5d460b82ba215cffa6805
ConAntares <LukeNiu@outlook.com>
2019-05-10 02:41:02 +0800

    Update

--- Approach/Dict in Julia.jl
+++ Approach/Dict in Julia.jl
@@ -1,11 +1,13 @@
 #### Dict
 
-re = Dict(1=>1.1, 2=>2.2, 3=>3.3)
+re = Dict(1=>1.1, 2=>2.2, 3=>3.3, 4=>4.4)
 println(re)
-    # Dict(2=>2.2,3=>3.3,1=>1.1)
+    # Dict(4=>4.4,2=>2.2,3=>3.3,1=>1.1)
 println(typeof(re))
     # Dict{Int64,Float64}
 
 re = Dict(i=>i^2 for i in 1:4)
 println(re)
     # Dict(4=>16,2=>4,3=>9,1=>1)
+println(re[1]," ",re[2]," ",re[3]," ",re[4])
+    # 1 4 9 16
---
+++ Approach/Dict in Python.py
@@ -0,0 +1,15 @@
+#### Dict
+
+import numpy as np
+
+re = {1:1.1, 2:2.2, 3:3.3, 4:4.4}
+print(re)
+    # {1: 1.1, 2: 2.2, 3: 3.3}
+print(type(re))
+    # <class 'dict'>
+
+re = {i:i**2 for i in np.arange(1,5,1)}
+print(re)
+    # {1: 1, 2: 4, 3: 9, 4: 16}
+print(re[1],re[2],re[3],re[4])
+    # 1 4 9 16
