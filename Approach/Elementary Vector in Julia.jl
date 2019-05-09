a35c4156006d6de179ac007f8f24d072f6b8d399
ConAntares <LukeNiu@outlook.com>
2019-05-09 23:57:36 +0800

    Update

--- Approach/Elementary Vector in Julia.jl
+++ Approach/Elementary Vector in Julia.jl
@@ -22,7 +22,13 @@ C = float([0,1,2,3])
 println(C)          # [0.0, 1.0, 2.0, 3.0]
 
 D = range(1, stop = 10, step = 2)
-println(D)            # 1:2:9
+println(D)          # 1:2:9
 
 E = range(1, stop = 11, length = 51)
-println(E)            # 1.0:0.2:11.0
+println(E)          # 1.0:0.2:11.0
+
+F = zeros(10)
+println(F)          # [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+G = ones(10)
+println(G)          # [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
