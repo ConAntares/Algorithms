f73c2a949f2845952b11fb953635aa5e98fef58f
ConAntares <LukeNiu@outlook.com>
2019-05-09 23:28:40 +0800

    Update

--- Approach/Elementary Array in Julia.jl
+++ Approach/Elementary Array in Julia.jl
@@ -1,12 +1,22 @@
-
 #### Elementary Array
 
 A = [0,1,2,3,4,5,6,7,8,9]
 
-println(A)      # [1, 2, 3, 4, 5]
-println(A[1])   # 0
-println(A[1:5]) # [0, 1, 2, 3, 4]
-println(A[5:1]) # int64()
+println(A)          # [1, 2, 3, 4, 5]
+println(A[1])       # 0
+println(A[1:5])     # [0, 1, 2, 3, 4]
+println(A[5:1])     # int64()
+println(typeof(A))  # Array{Int64,1}
+println(eltype(A))  # Int64
+println(length(A))  # 10
+println(ndims(A))   # 1
+println(size(A))    # (10,)
+println(size(A,1))  # 10
+println(size(A,2))  # 1
+println(size(A,3))  # 1
 
 B = float(A)
-println(B)      # [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+println(B)          # [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+
+C = float([0,1,2,3])
+println(C)          # [0.0, 1.0, 2.0, 3.0]
---
+++ Approach/Elementary Array in Python.py
@@ -0,0 +1,21 @@
+#### Elementary Array
+
+import numpy as np
+
+A = np.array([0,1,2,3,4,5,6,7,8,9])
+print(A)        # [0 1 2 3 4 5 6 7 8 9]
+print(A[1])     # 1
+print(A[1:5])   # [1 2 3 4]
+print(A[5:1])   # []
+print(len(A))   # 10
+print(type(A))  # <class 'numpy.ndarray'>
+print(A.dtype)  # int32
+print(A.ndim)   # 1
+print(A.shape)  # (10,)
+print(A.nbytes) # 40
+
+B = np.float64(A)
+print(B)        # [0. 1. 2. 3. 4. 5. 6. 7. 8. 9.]
+
+C = np.array([0,1,2,3,4,5,6,7,8,9],dtype=np.float64)
+print(C)        # [0. 1. 2. 3. 4. 5. 6. 7. 8. 9.]
