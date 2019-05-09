ef3a29a735128b5bfb54a58309a068b060b80d0f
ConAntares <LukeNiu@outlook.com>
2019-05-10 03:04:35 +0800

    Update

--- Approach/Tuple in Julia.jl
+++ Approach/Tuple in Julia.jl
@@ -1,6 +1,17 @@
 #### Tuple
 
 re = (1,2,3,4)
+println(re)
+    # (1, 2, 3, 4)
 println(typeof(re))
     # NTuple{4,Int64}
 
+re = ("a","b","c","d")
+println(re)
+    # ("a", "b", "c", "d")
+println(typeof(re))
+    # NTuple{4,String}
+
+re = Tuple([1,2,3,4])
+println(re)
+    # (1, 2, 3, 4)
