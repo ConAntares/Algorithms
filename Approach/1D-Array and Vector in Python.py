d22361911409c6e18949ec7af981e5cb9148cfb1
ConAntares <LukeNiu@outlook.com>
2019-05-10 21:01:04 +0800

    Update

--- Approach/1D-Array and Vector in Python.py
+++ Approach/1D-Array and Vector in Python.py
@@ -2,6 +2,40 @@
 
 import numpy as np
 
+## Summary of NumPy functions for generating arrays
+"""
+np.array:
+    Creates an array for which the elements are given by an array-like object, which,
+    for example, can be a (nested) Python list, a tuple, an iterable sequence,
+    or another ndarray instance.
+np.zero:
+    Creates an array – with the specified dimensions and data type – that is filled with zeros.
+np.ones:
+    Creates an array – with the specified dimensions and data type – that is filled with ones.
+np.diag:
+    Creates an array with evenly spaced values between specified start, end, and, increment values.
+np.linspace:
+    Creates an array with evenly spaced values between specified start and end values,
+    using a specified number of elements.
+np.logspace:
+    Creates an array with values that are logarithmically spaced between the given start and end values.
+np.meshgrid:
+    Generate coordinate matrices (and higher-dimensional coordinate arrays) from onedimensional coordinate vectors.
+np.fromfunction:
+    Create an array and fill it with values specified by a given function,
+    which is evaluated for each combination of indices for the given array size.
+np.fromfile:
+    Create an array with the data from a binary (or text) file.
+    NumPy also provides a corresponding function np.tofile with which NumPy arrays can be stored to disk,
+    and later read back using np.fromfile.
+np.loadtxt, np.genfromtxt:
+    Creates an array from data read from a text file. For example, a comma-separated value. (CSV) file.
+    The function np.genfromtxt also supports data files with missing values.
+np.random.rand:
+    Generates an array with random numbers that are uniformly distributed between 0 and 1.
+    Other types of distributions are also available in the np.random module.
+"""
+
 A = np.array([0,1,2,3,4,5,6,7,8,9])
 print(A)        # [0 1 2 3 4 5 6 7 8 9]
 print(A[1])     # 1
