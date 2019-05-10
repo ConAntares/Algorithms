afa12c1f4b2bd2bbb51373ee3ce38f880b61626b
ConAntares <LukeNiu@outlook.com>
2019-05-10 20:57:56 +0800

    Update

--- Approach/2D-Array and Matrix in Julia.jl
+++ Approach/2D-Array and Matrix in Julia.jl
@@ -7,4 +7,9 @@ A =
     5 3 1 4;
 ]
 
-println(eltype(A))     # Int64
+println(eltype(A))      # Int64
+println(length(A))      # 12
+println(ndims(A))       # 2
+println(size(A))        # (3,4)
+println(size(A,1))      # 3
+println(size(A,2))      # 4
