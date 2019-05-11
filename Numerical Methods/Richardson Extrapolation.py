24ed361c9c4d5d1ded9212119a1f230db682c6eb
ConAntares <LukeNiu@outlook.com>
2019-05-12 00:22:27 +0800

    Update

--- Numerical Methods/Richardson Extrapolation.py
+++ Numerical Methods/Richardson Extrapolation.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 Sn = 1 + 1/2^2 + 1/3^2 + 1/4^2 + ... + 1/n^2
 lim(n->∞)Sn = π^2/6 ≈ 1.6449340668482264
 
-R1(n) = ((n+1)*S(n+1) - n*S(n))/1!
-R2(n) = ((n+2)^2*S(n+1)-2*(n+1)^2*S(n+1)+n^2*S(n))/2!
+R1(n) = ((n+1)*S(n+1) - n*S(n))//np.math.factorial(1)
+R2(n) = ((n+2)^2*S(n+1)-2*(n+1)^2*S(n+1)+n^2*S(n))/np.math.factorial(2)
+……
 """
+
+
