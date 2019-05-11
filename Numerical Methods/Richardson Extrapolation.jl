3c37d4b0bc5e8a31302721f31eb4ac5564647cbb
ConAntares <LukeNiu@outlook.com>
2019-05-12 01:21:41 +0800

    Update

--- Numerical Methods/Richardson Extrapolation.jl
+++ Numerical Methods/Richardson Extrapolation.jl
@@ -21,30 +21,39 @@ Y = view(A,:,2)
 
 count = length(X)
 
-R1 = ones(count)
-for i in range(1; stop=count)
-    if i in 1:count - 1
-        R1[i] = ((i+1)*Y[i+1]-i*Y[i])/factorial(1)
-    else
-        R1[i] = 0
+function RE1(Y)
+    R1 = ones(count)
+    for i in range(1; stop=count)
+        if i in 1:count - 1
+            R1[i] = ((i+1)*Y[i+1]-i*Y[i])/factorial(1)
+        else
+            R1[i] = 0
+        end
+        i = i + 1
     end
-    i = i + 1
+    return R1
 end
 
-R2 = ones(count)
-for i in range(1; stop=count)
-    if i in 1:count - 2
-        R2[i] = ((i+2)^2*Y[i+2]-2*(i+1)^2*Y[i+1]+i^2*Y[i])/factorial(2)
-    else
-        R2[i] = 0
+function RE2(Y)
+    R2 = ones(count)
+    for i in range(1; stop=count)
+        if i in 1:count - 2
+            R2[i] = ((i+2)^2*Y[i+2]-2*(i+1)^2*Y[i+1]+i^2*Y[i])/factorial(2)
+        else
+            R2[i] = 0
+        end
+        i = i + 1
     end
-    i = i + 1
+    return R2
 end
 
-plot(fontfamily=("Serif"),dpi=512)
-scatter!(X,Y, color="#B4B4B4",marker=(:circle,10,Plots.stroke(:white)),label="Source")
-plot!(X[1:count-1],R1[1:count-1],color="#F0140A",label="Foreast")
-plot!(X[1:count-2],R2[1:count-2],color="#00A0DC",label="Foreast")
+U1 = RE1(Y)
+U2 = RE2(Y)
 
 td = time() - to
 println("The time interval is $td s.")
+
+plot(fontfamily=("Serif"),dpi=512)
+scatter!(X,Y, color="#B4B4B4",marker=(:circle,10,Plots.stroke(:white)),label="Source")
+plot!(X[1:count-1],U1[1:count-1],color="#F0140A",label="Foreast")
+plot!(X[1:count-2],U2[1:count-2],color="#00A0DC",label="Foreast")
