829d9185b21e35f92e137f74bb7c114e613a3db1
ConAntares <LukeNiu@outlook.com>
2019-05-12 01:51:12 +0800

    update

--- Numerical Methods/Richardson Extrapolation.py
+++ Numerical Methods/Richardson Extrapolation.py
@@ -1,6 +1,7 @@
 #### Richardson Extrapolation
 
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 
 """
@@ -12,6 +13,41 @@ R2(n) = ((n+2)^2*S(n+1)-2*(n+1)^2*S(n+1)+n^2*S(n))/np.math.factorial(2)
 ……
 """
 
+to = time.time()
+
 X,Y = np.loadtxt('Data/Td.dat', unpack=True)
 
 count = len(X)
+
+def RE1(Y):
+    R1 = np.ones(count)
+    for i in range(0, count - 1):
+        if i < count - 1 :
+            R1[i] = ((i+1)*Y[i+1]-i*Y[i])/np.math.factorial(1)
+        else:
+            R1[i] = 0
+        i = i + 1
+    return R1
+
+def RE2(Y):
+    R2 = np.ones(count)
+    for i in range(0, count - 2):
+        if i < count - 2 :
+            R2[i] = ((i+2)**2*Y[i+2]-2*(i+1)**2*Y[i+1]+i**2*Y[i])/np.math.factorial(2)
+        else:
+            R2[i] = 0
+        i = i + 1
+    return R2
+
+U1 = RE1(Y)
+U2 = RE2(Y)
+
+td = time.time() - to
+print("The time interval is $td s.")
+
+plt.rcParams['font.family'] = 'CMU Serif'
+plt.plot(X, Y, "o", c="#B4B4B4", alpha=0.9)
+plt.plot(X[0:len(U1)-1], U1[0:len(U1)-1], c="#F0140A", alpha=0.9)
+plt.plot(X[0:len(U1)-2], U2[0:len(U1)-2], c="#FFC814", alpha=0.9)
+plt.tick_params(direction='in')
+plt.show()
---
+++ Numerical Methods/Test_IF.py
@@ -0,0 +1,13 @@
+#### IF
+
+def iif(i):
+    if i >= 1 and i <= 3:
+        print("I")
+    else:
+        print("O")
+
+iif(0)  # O
+iif(1)  # I
+iif(2)  # I
+iif(3)  # I
+iif(4)  # O
