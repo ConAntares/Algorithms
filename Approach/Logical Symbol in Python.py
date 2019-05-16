6f1bc362ad70ea5e4106f8c700f11c61c4c615cf
ConAntares <LukeNiu@outlook.com>
2019-05-16 15:05:42 +0800

    Update

--- Approach/Logical Symbol in Python.py
+++ Approach/Logical Symbol in Python.py
@@ -1,15 +1,60 @@
 ####  Logical Symbol
 
-str = (1<2) and (2<3)
-print(str)              # True
-
-str = (1>2) and (2<3)
+## And
+str = False and False
 print(str)              # False
 
-str = (1>2) or (2<3)
-print(str)              # True
-
-str = (1>2) or (2>3)
+str = False and True
 print(str)              # False
 
-a = 3.14
+str = True and False
+print(str)              # False
+
+str = True and True
+print(str)              # True
+
+## Or
+str = False or False
+print(str)              # False
+
+str = False or True
+print(str)              # True
+
+str = True or False
+print(str)              # True
+
+str = True or True
+print(str)              # True
+
+## Not
+str = not True
+print(str)              # False
+
+str = not False
+print(str)              # True
+
+## Nand
+str = not (False and False) 
+print(str)              # True
+
+str = not (False and True) 
+print(str)              # True
+
+str = not (True and False) 
+print(str)              # True
+
+str = not (True and True) 
+print(str)              # False
+
+## Xor
+str = (not (False and False)) and (False or False)
+print(str)              # False
+
+str = (not (False and True)) and (False or True)
+print(str)              # True
+
+str = (not (True and False)) and (True or False)
+print(str)              # True
+
+str = (not (True and True)) and (True or True)
+print(str)              # False
