10d5db46af6638858d04287e64d697bf23489991
ConAntares <LukeNiu@outlook.com>
2019-05-19 04:02:10 +0800

    Update

---
+++ Linear Algebra/Eigenvalue.py
@@ -0,0 +1,28 @@
+#### Eigenvalue and Eigenvector
+
+import numpy as np
+
+"""
+A·V = λ·V
+λ: Eigenvalue
+V: Eigenvector
+"""
+
+A = np.array([[3.14, 1.59, 2.65],
+              [2.71, 8.28, 1.82],
+              [6.62, 6.06, 8.96]])
+
+print("The Source Matrix：\n{}".format(A))
+
+λ, V = np.linalg.eig(A)
+
+print("The eigenvalue：\n{}" .format(λ))
+print("The eigenvector：\n{}".format(V))
+
+for n in range(0,len(V)):
+    val = λ[n]
+    vec = V[:,n]
+    print("The eigenvalue: %f corresponds eigenvector:" %val, vec, ";")
+    lef = np.dot(A,  vec)
+    rig = np.dot(val,vec)
+    print("Then, A·V equals:", lef, "and λ·V equals:", rig, ".")
