882be36f66bd6b0ab2b7ca9f03109e713bcbd804
ConAntares <LukeNiu@outlook.com>
2019-05-23 19:29:25 +0800

    Update

--- Deep Learning and Machine Learning/Perceptron/Simple Perceptron.py
+++ Deep Learning and Machine Learning/Perceptron/Simple Perceptron.py
@@ -1,6 +1,8 @@
 #### Simple Perceptron
 
-# 1.Received an AND function with parameters (x1, x2)
+import numpy as np
+
+# Received an AND function with parameters (x1, x2)
 def AND(x1,x2):
     w1 = 0.5
     w2 = 0.5
@@ -32,4 +34,30 @@ print(OR(0,1))     # 0
 print(OR(1,0))     # 0
 print(OR(1,1))     # 1
 
+def NOT(x):
+    w = 0.5
+    θ = 0.25
+    t = x * w
+    if t <= θ:
+        return 1
+    elif t > θ:
+        return 0
+
+print(NOT(0))     # 1
+print(NOT(1))     # 0
+
 ## Weight and Bias
+
+x = np.array([0.0,1.0])     # Input
+w = np.array([0.5,0.5])     # Weight
+b = -0.75                   # Bias
+
+re = w*x
+print(re)
+    # [0.  0.5]
+re = np.sum(w*x) + b
+print(re)
+    # -0.25
+
+def AND_plus(x1, x2):
+    
