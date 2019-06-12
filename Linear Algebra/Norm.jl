f9d5cafd65456e6825ef0e8f22ccc5af59ae1533
ConAntares <LukeNiu@outlook.com>
2019-06-13 02:48:12 +0800

    Update

--- Linear Algebra/Norm.jl
+++ Linear Algebra/Norm.jl
@@ -2,18 +2,28 @@
 
 using LinearAlgebra
 
-# Definition:
+# Norm
 """
-‖A‖ = sqrt(ΣiΣj(|A[i,j]|^2))
-"""
-# Properties
-"""
-‖A‖ ≥ 0
-‖A‖ = 0 iff A = O
-‖aA‖ = |a| * ‖A‖
-‖A + B‖ ≤ ‖A‖ + ‖B‖
+Definition
+    ‖A‖ = sqrt(ΣiΣj(|A[i,j]|^2))
+Properties
+    ‖A‖ ≥ 0
+    ‖A‖ = 0 iff A = O
+    ‖aA‖ = |a| * ‖A‖
+    ‖A + B‖ ≤ ‖A‖ + ‖B‖
 """
 
-x = [2,-1, 2]
-r = norm(x)
+A = [2,-1, 2]
+r = norm(A)
 println(r)      # 3.0
+
+# RMS Value
+"""
+Definition
+    rms(A) = ‖A‖/sqrt(n)
+where n means the number of elements in Array A
+"""
+
+rms(x) = norm(x) / sqrt(length(x))
+r = rms(A)
+println(r)      # 1.7320508075688774
--- Linear Algebra/Norm.py
+++ Linear Algebra/Norm.py
@@ -3,18 +3,30 @@
 import math
 import numpy as np
 
-# Definition:
+# Norm
 """
-‖A‖ = sqrt(ΣiΣj(|A[i,j]|^2))
-"""
-# Properties
-"""
-‖A‖ ≥ 0
-‖A‖ = 0 iff A = O
-‖aA‖ = |a| * ‖A‖
-‖A + B‖ ≤ ‖A‖ + ‖B‖
+Definition
+    ‖A‖ = sqrt(ΣiΣj(|A[i,j]|^2))
+Properties
+    ‖A‖ ≥ 0
+    ‖A‖ = 0 iff A = O
+    ‖aA‖ = |a| * ‖A‖
+    ‖A + B‖ ≤ ‖A‖ + ‖B‖
 """
 
-x = np.array([2,-1, 2])
-r = np.linalg.norm(x)
+A = np.array([2,-1, 2])
+r = np.linalg.norm(A)
 print(r)        # 3.0
+
+# RMS Value
+"""
+Definition
+    rms(A) = ‖A‖/sqrt(n)
+where n means the number of elements in Array A
+"""
+
+def rms(x):
+    return np.linalg.norm(x) / math.sqrt(x.size)
+
+r = rms(A)
+print(r)        # 1.7320508075688774
