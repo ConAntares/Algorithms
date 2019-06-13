9c4ea2a71eeaa491c9b682f0029bf0f53bcc4a6a
ConAntares <LukeNiu@outlook.com>
2019-06-13 22:16:00 +0800

    Update

---
+++ Linear Algebra/Correlation Coefficient.py
@@ -0,0 +1,12 @@
+#### Correlation Coefficient
+
+import math
+import numpy as np
+
+# About Correlation Coefficient
+"""
+The correlation between two vectors A and B (with nonzero standard deviation) is defined as:
+    ρ = Â'B̂ / (‖Â‖*‖B̂‖)
+Where Â and B̂ are the de-meaned versions of A and B, respectively.
+"""
+
