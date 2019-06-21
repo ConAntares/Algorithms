7449a937f0d8e9b06e001c84dbd0143761940055
ConAntares <LukeNiu@outlook.com>
2019-06-22 02:59:21 +0800

    Update

---
+++ Linear Algebra/Matrix Elimination.jl
@@ -0,0 +1,10 @@
+#### Matrix Elimination
+
+using LinearAlgebra
+
+# Input a n*n array as the determinant you want to analysis.
+D = [ 4 1 5 4 3;
+      3 4 6 2 5;
+      2 7 5 1 3;
+      1 6 4 3 6;
+      2 1 6 5 4 ]
