fdd9d739a60144dab60e9fa31171590b74a0229d
ConAntares <LukeNiu@outlook.com>
2019-07-03 01:28:43 +0800

    Update

--- Data Visualization Temples/Julia Gaston Sample.jl
+++ Data Visualization Temples/Julia Gaston Sample.jl
@@ -4,4 +4,6 @@ using Gaston
 
 t = -4:0.01:4
 
-plot(t, sinc.(t), font="Cambria,12", plotstyle="lines", legend="Function")
+figurename = "JuliaGnuplot"
+postcomm = "set terminal cairolatex standalone; set output '$figurename.tex'"
+plot(t, sinc.(t), font="Cambria,12", plotstyle="lines", legend="Function", gpcom=postcomm)
--- Data Visualization Temples/Julia Gnuplot Sample.jl
+++ Data Visualization Temples/Julia Gnuplot Sample.jl
@@ -5,14 +5,8 @@ using Gnuplot
 X = range(-2pi, stop=2pi, length=100)
 Y = 1.5 .* sin.(0.3 .+ 0.7X)
 
-@gp(
-    title = "My title",
-    xrange = (-7,7),    yrange = (-4,4),
-    xlabel = "X label", ylabel = "Y label", 
-    # "set output",
-    X, Y, "with lines",
-    save(term="cairolatex standalone", output="JuliaGnuplot.tex")
-);
-
-# @gp tit="My title" xr=(-7,7) yr=(-4,4) xlab="X label" ylab="Y label" X Y "w l"
-# save(term="cairolatex standalone", output="JuliaGnuplot.tex")
+figurename = "JuliaGnuplot"
+@gp(title="My title",
+    xrange=(-7,7), yrange=(-4,4), xlabel="X label", ylabel="Y label", 
+    X, Y, "with lines", );
+save(term="cairolatex standalone", output="$figurename.tex")
