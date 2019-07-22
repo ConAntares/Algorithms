c2beeb8445d887308db417f1bbc4e1f9e14c53c9
ConAntares <LukeNiu@outlook.com>
2019-07-23 03:00:44 +1000

    Update

--- Data Visualization Temples/Julia Gaston Sample.jl
+++ Data Visualization Temples/Julia Gaston Sample.jl
@@ -6,5 +6,5 @@ X = range(-2pi, stop=2pi, length=100)
 Y = 1.5 .* sin.(0.3 .+ 0.7X)
 
 figurename = "JuliaGnuplot"
-# plotcom = "set terminal cairolatex standalone; set output '$figurename.tex'"
+#plotcom = "set terminal cairolatex standalone; set output '$figurename.tex'"
 plot(X, Y, font="Cambria,12", plotstyle="lines", legend="Function", gpcom=plotcom)
