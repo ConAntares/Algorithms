a710d88779c95569fce59a041e2a1cd9782c2788
ConAntares <LukeNiu@outlook.com>
2019-08-04 02:31:55 +1000

    Update

--- Data Visualization Templates/Julia Gnuplot Sample.jl
+++ Data Visualization Templates/Julia Gnuplot Sample.jl
@@ -9,4 +9,4 @@ figurename = "JuliaGnuplot"
 @gp(title="My title",
     xrange=(-7,7), yrange=(-4,4), xlabel="X label", ylabel="Y label",
     X, Y, "with lines", );
-save(term="cairolatex standalone", output="$figurename.tex")
+# save(term="cairolatex standalone", output="$figurename.tex")
