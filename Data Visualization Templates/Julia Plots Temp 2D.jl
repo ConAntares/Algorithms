80ee54f07d6f6c75e734e2536aab103708eb0a2b
ConAntares <LukeNiu@outlook.com>
2019-08-05 03:26:27 +1000

    Update

--- Data Visualization Templates/Julia Plots Temp 2D.jl
+++ Data Visualization Templates/Julia Plots Temp 2D.jl
@@ -12,7 +12,7 @@ X = range(-pi*pi, pi*pi, length=100)
 Y = pi/2 .* sin.(1/2 .+ (pi/4)X)
 
 plot(dpi=256, gridls=:dash, legend=:topright, frame=:both,
-     fgborder="#505050",fglegend="#A0A0A0", fgaxis="#505050", tickfontsize=10, legendfontsize=10)
+    fgborder="#505050",fglegend="#A0A0A0", fgaxis="#505050", tickfontsize=10, legendfontsize=10)
 plot!(X,Y, title = L"Title $\alpha$", lw=2, label=L"wave $f=f(x)$", annotation=(1, 1, L"Annotation $\beta$"))
 
 # Plots.savefig("Plots.pdf")
