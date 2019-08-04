12f8d1f5c9f36f31a9a67992894c0cd4dc36f0a1
ConAntares <LukeNiu@outlook.com>
2019-08-04 19:48:11 +1000

    Update

--- Data Visualization Templates/Julia Pyplot Temp 2D.jl
+++ Data Visualization Templates/Julia Pyplot Temp 2D.jl
@@ -1,1 +1,18 @@
-#### 2D Plotting with Matplotlib in Python
+#### 2D Plotting with Matplotlib in Pythonusing PyPlot
+
+using PyPlot
+
+PyPlot.rc("text", usetex = "true")
+PyPlot.rc("font", family = "CMU Serif")
+
+fig = figure(figsize=(12,8))
+# fig.suptitle("SupTitle",size=20)
+title(raw"Title $\alpha$",size=24)
+
+X = range(-2pi, stop=2pi, length=100)
+Y = 1.5 .* sin.(0.3 .+ 0.7X)
+
+plot(X,Y,lw=4, label = raw"Function $f=f(x)$")
+legend(loc="best")
+grid(true, linestyle="--", color="#D0D0D0", lw=1, alpha=0.5)
+tick_params(direction="in", top=true, right=true, bottom=true, left=true, which="both")
